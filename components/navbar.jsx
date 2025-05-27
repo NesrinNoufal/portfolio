f38b41e70f3de028f022b23f5f2a2c4f1d002edf
NesrinTechfriar <nesrin@techfriar.com>
2025-05-27 23:37:57 +0530

    navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
+import {assets} from '../assets/assets'
 
-export default function navbar() {
+export default function Navbar() {
   return (
     <div>
-
+        <nav className='w-full fixed'></nav>
         
     </div>
   )

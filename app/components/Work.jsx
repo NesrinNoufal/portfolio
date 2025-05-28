29c7c111fa4d558f967db280c9a3f017ab8b757e
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-05-28 23:02:17 +0530

    work component update

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -24,11 +24,13 @@ export default Work = () => {
                 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between
                 duration-500 group-hover:bottom-7'>
                     <div>
-                        <h2 className>{project.title}</h2>
-                        <p>{project.description}</p>
+                        <h2 className='font-semibold'>{project.title}</h2>
+                        <p className='text-sm text-gray-700'>{project.description}</p>
                     </div>
-                    <div>
-                        <Image src={assets.send_icon} alt='' className='w-5'/>
+                    <div className='border rounded-full border-black w-9
+                    aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300
+                    transition'>
+                        <Image src={assets.send_icon} alt='send-icon' className='w-5'/>
                     </div>
                 </div>
             </div>

42402ecab83df6c38e991f5db6d2fd299fe21eec
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-05-29 23:32:54 +0530

    contact and work section update

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,3 +1,4 @@
+import { assets } from '@/assets/assets'
 import React from 'react'
 
 export default Work = () => {
@@ -40,6 +41,10 @@ export default Work = () => {
 
         ))}
        </div>
+       <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px]
+       border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
+        Show more <Image src={assets.right_arrow_bold} alt="Right arrow" className='w-4'/>
+       </a>
 
     </div>
   )

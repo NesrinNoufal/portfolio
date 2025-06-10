7aaa4959a72bb1fa98f6b0615bbbad1e94b4a6a9
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-06-10 19:56:28 +0530

    resume file updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -64,7 +64,7 @@ const Header = ({ isDarkMode }) => {
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
           viewport={{ once: true }}
-          href="/nesrinnoufal_resume.pdf"
+          href="/nesrinnoufalResume.pdf"
           download
           className="btn resume-btn"
         >

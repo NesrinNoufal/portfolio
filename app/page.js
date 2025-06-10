1ded527abb7a05e93f2089237f972fdfd71ebcd2
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-06-10 11:48:30 +0530

    footer path error correction

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import About from "./components/About"
 import Services from "./components/Services"
 import Work from "./components/Work"
 import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import Footer from "./components/footer"
 
 
 export default function Home() {

9636d7072be1ab8983622f5c859b18194f4d7ec4
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-06-10 11:41:02 +0530

    footer path error fix

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ import Work from "./components/Work"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 
+
 export default function Home() {
 
   const[isDarkMode,setIsDarkMode]=useState(false);

377e96799bb27a820d1949640de627ed527ad475
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-06-18 23:29:21 +0530

    dark mode set as default state

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Footer from "./components/footer"
 
 export default function Home() {
 
-  const[isDarkMode,setIsDarkMode]=useState(false);
+  const[isDarkMode,setIsDarkMode]=useState(true);
 
   useEffect(()=>{
     if(localStorage.theme==='dark' || (!('theme' in localStorage) && window.matchMedia(
